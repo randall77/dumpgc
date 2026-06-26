b0806899fdedc8de3860dadb6abac784e4ae7858
Keith Randall <khr@golang.org>
2026-06-26 15:11:56 -0700

    fix for upcoming 1.27

--- dumpgc.go
+++ dumpgc.go
@@ -439,12 +439,12 @@ type moduledata struct {
 	covctrs, ecovctrs          uintptr
 	end, gcdata, gcbss         uintptr
 	types, typedesclen, etypes uintptr
+	itaboffset, itabsize       uintptr
 	rodata                     uintptr
 	gofunc                     uintptr // go.func.*
 	epclntab                   uintptr
 
 	textsectmap slice[textsect]
-	itablinks   slice[pointer[itab]]
 
 	ptab slice[ptabEntry]
 
